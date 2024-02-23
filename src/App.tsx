186108b69d7d104f39e55eae4c48eb28268b66fd
Alexander Andersen <alexander.andersen@adaptagency.com>
2024-02-23 19:42:07 +0100

    daisy setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css'
+import { Button } from 'react-daisyui'
 
 function App() {
-  return <div>Hello world</div>
+  return (
+    <div className={'w-full h-full flex justify-center items-center py-10'}>
+      <div>
+        <h1 className={'text-3xl'}>Scoreboard</h1>
+        <Button>Hello</Button>
+      </div>
+    </div>
+  )
 }
 
 export default App
