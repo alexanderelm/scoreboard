fb35004553e21a64a13ecaf405b671c48dc36ddd
Alexander Andersen <alexander.andersen@adaptagency.com>
2024-02-25 18:03:05 +0100

    remove app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LeaderboardPage from './pages/leaderboard.tsx'
+import ResultsPage from './pages/results.tsx'
+import { Nav } from './components/nav'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LeaderboardPage />,
+  },
+  {
+    path: '/results',
+    element: <ResultsPage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Nav />
+
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
